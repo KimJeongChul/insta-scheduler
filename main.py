762922aa3dd98ed34b2eed17207c916170e775ae
KimJeongChul <kjc5443@gmail.com>
2023-08-27 21:57:56 +0900

    [feat] main.py v1.0.0

---
+++ main.py
@@ -0,0 +1,34 @@
+import schedule
+import time
+from datetime import datetime
+from instagrapi import Client
+
+def post_job():
+    str_time = datetime.now().strftime('%Y%m%d')
+    file_path = "./" + str_time + "/job"
+    
+    print("[*] ---------------------------- day: ", str_time)
+    with open(file_path, 'r') as job_file:
+        lines = job_file.readlines()
+        for job in lines:
+            job_info = job.split('  ')
+            id = job_info[0]
+            password = job_info[1]
+            img_path = job_info[2]
+            msg = job_info[3]
+            msg.replace('\\n', '\n')
+            
+            print('id": ', id, ' password: ', password, ' img_path: ', img_path)
+            print('msg: ', msg)
+    
+            cl = Client()
+            cl.login(id, password)
+
+            media = cl.photo_upload(img_path, msg)
+            print('media: ', media.dict())
+
+schedule.every().day.at('21:28:00').do(post_job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
